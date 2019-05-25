668164eb275def8632e14b0ad85de991417ad4fb
Matt Wright <mattwright3195@gmail.com>
2019-05-24 18:32:04 -0700

    purge setup

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,7 +1,27 @@
-const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project 
+  content: [
+    './src/**/*.html',
+    './src/**/*.jsx',
+    // etc.
+  ],
+
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
 
 module.exports = {
-    plugins: [
-        tailwindcss('./tailwind.config.js'),
-    ],
-};
+  plugins: [
+    require('tailwindcss'),
+    purgecss
+  ]
+}
+
+// module.exports = {
+//   plugins: [
+//     require('tailwindcss'),
+//     ...process.env.NODE_ENV === 'production'
+//       ? [purgecss]
+//       : []
+//   ]
+// }
--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return (<>
-    <div className="bg-green-400">
+    <div className="divtesting text-white">
       Hello World!
     </div></>
   );
