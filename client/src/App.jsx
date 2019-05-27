9583e6ccf38ecf75bf5cf9c0d71c6a41bec42b1e
Matt Wright <mattwright3195@gmail.com>
2019-05-26 22:20:57 -0700

    drag working not well

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,29 +3,49 @@ import React from 'react';
 import { useMousePosition } from '@react-hook/mouse-position';
 
 const App = () => {
-  const [mousePositionRef, mousePosition] = useMousePosition(0, 0, 120);
+  const [mousePositionRef, mousePosition] = useMousePosition(0, 0, 60);
   const [scale, setScale] = React.useState(1);
-  const [translate] = React.useState({ x: 0, y: 0 });
   const [points, setPoints] = React.useState([100, 100, 100, 900, 900, 900, 900, 100]);
-
-  // console.log(mousePosition.x, mousePosition.y);
+  const [drawing, setDrawing] = React.useState(false);
+  const [panning, setPanning] = React.useState(false);
+  const [translate, setTranslate] = React.useState({ x: 0, y: 0 });
+  const [origin, setOrigin] = React.useState({ x: 0, y: 0 });
   // console.log(mousePosition.elementHeight, mousePosition.elementWidth);
-
-  const handleMouseUp = () => {
-    setPoints(prevPoints => [...prevPoints, mousePosition.x, mousePosition.y]);
-  };
 
   const handleWheel = e => {
     if (e.deltaY < 0) {
       setScale(prevScale => prevScale + 1);
     } else {
       setScale(prevScale => {
-        if (prevScale === 1) {
+        if (prevScale <= 1) {
+          // parseFloat('0.5');
           return prevScale;
         }
         return prevScale - 1;
       });
     }
+  };
+
+  const handleMouseDown = () => {
+    setOrigin({ x: mousePosition.x, y: mousePosition.y });
+    setPanning(true);
+  };
+
+  const handleMouseMove = e => {
+    e.preventDefault();
+    if (!panning) return;
+
+    const xDiff = origin.x - mousePosition.x;
+    const yDiff = origin.y - mousePosition.y;
+
+    setOrigin({ x: mousePosition.x, y: mousePosition.y });
+    setTranslate({ x: translate.x - xDiff, y: translate.y - yDiff });
+  };
+
+  const handleMouseUp = e => {
+    e.preventDefault();
+    setPanning(false);
+    // setOrigin({ x: 0, y: 0 });
   };
 
   return (
@@ -39,7 +59,9 @@ const App = () => {
           height="1000"
           className="bg-white"
           ref={mousePositionRef}
-          onMouseUp={handleMouseUp}
+          onMouseDown={handleMouseDown}
+          onMouseMove={e => handleMouseMove(e)}
+          onMouseUp={e => handleMouseUp(e)}
           onWheel={handleWheel}
         >
           <g id="canvas" transform={`translate(${translate.x},${translate.y}) scale(${scale})`}>
@@ -47,15 +69,17 @@ const App = () => {
               <polyline fill="none" points={points} stroke="#000" strokeWidth="6" />
             </g>
             <g>
-              <line
-                x1={points[points.length - 2]}
-                y1={points[points.length - 1]}
-                x2={mousePosition.x}
-                y2={mousePosition.y}
-                strokeDasharray="5, 5"
-                strokeWidth="2"
-                stroke="#006BF7"
-              />
+              {drawing && (
+                <line
+                  x1={points[points.length - 2]}
+                  y1={points[points.length - 1]}
+                  x2={mousePosition.x}
+                  y2={mousePosition.y}
+                  strokeDasharray="5, 5"
+                  strokeWidth="2"
+                  stroke="#006BF7"
+                />
+              )}
             </g>
           </g>
         </svg>
